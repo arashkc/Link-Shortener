02e0c872dafa95acac1ffeadc5b95e28e6daec62
arashkc <140426464+arashkc@users.noreply.github.com>
2024-05-28 14:35:10 +0330

    css updated

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div id="body">
+      <div id="userInput">
+        <input type="text" placeholder="Example: https://www.google.com" />
+        <button>Shorten</button>
+      </div>
+      <div id="resultArea"></div>
+    </div>
+  );
+}
+
+export default App;
